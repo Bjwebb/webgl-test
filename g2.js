41c414eb51ee630327b7a39babd358ab5581dc5a
Ben Webb <bjwebb67@googlemail.com>
2013-08-23 20:54:34 +0100

    Build simple collision detection without physijs

--- g2.js
+++ g2.js
@@ -1,7 +1,3 @@
-Physijs.scripts.worker = 'physijs_worker.js';
-Physijs.scripts.ammo = 'ammo.js';
-
-
 
 
 function mouseClick(e) {
@@ -9,19 +5,14 @@ function mouseClick(e) {
 }
 
 function mouseMove(e) { 
-    // .movementX/Y are valid normally as the mouse hovers
-    // over an element. But when the mouse is locked the
-    // mousemove event continues to fire whenever the mouse
-    // moves.
-  var movementX = e.movementX ||
+    var movementX = e.movementX ||
       e.mozMovementX          ||
       e.webkitMovementX       ||
       0,
-  movementY = e.movementY ||
+    movementY = e.movementY ||
       e.mozMovementY      ||
       e.webkitMovementY   ||
       0;
-    car.__dirtyRotation = true;
     car.rotateOnAxis( new THREE.Vector3(0,1,0), movementX/100 );
     camera_pivot.rotation.x -= movementY/100;
     if (camera_pivot.rotation.x < -Math.PI/4) camera_pivot.rotation.x = -Math.PI/4;
@@ -30,7 +21,7 @@ function mouseMove(e) {
 
 
 
-var car, scene, camera;
+var car, car2, scene, camera;
 function onLoad(){
     document.onkeydown = handleKeyDown;
     document.onkeyup = handleKeyUp;
@@ -48,7 +39,7 @@ document.getElementById('container').addEventListener("mousemove", mouseMove);
         container.appendChild( renderer.domElement );
 
         //Create a new scene
-        scene = new Physijs.Scene();
+        scene = new THREE.Scene();
            
 
     //Add controls for interactively moving the camera with mouse
@@ -69,10 +60,9 @@ document.getElementById('container').addEventListener("mousemove", mouseMove);
 
         //Load a model and store it in the variable XXX
         jsonLoader.load( "blender/test.json", function( geometry, materials ) { 
-            car = new Physijs.ConcaveMesh( geometry, new THREE.MeshFaceMaterial(materials) );
+            car = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials) );
             carpos = new THREE.Object3D();
             scene.add(car);
-            car.setDamping(0.8, 0.8);
 
             camera_pivot = new THREE.Object3D();
             car.add( camera_pivot );
@@ -83,25 +73,24 @@ document.getElementById('container').addEventListener("mousemove", mouseMove);
             camera.position.y = 5;
             camera.position.z = 5;
 
-            camera_pivot.eulerOrder = 'YXZ';
+            camera_pivot.rotation.order = 'YXZ';
                     
             camera_pivot.add( camera );
         });
             
         jsonLoader.load( "blender/car.json", function( geometry, materials ) { 
-            car2 = new Physijs.ConvexMesh( geometry, new THREE.MeshFaceMaterial(materials) );
-            car2.position.x = -3;
+            car2 = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials) );
+            car2.position.x = -5;
             scene.add(car2);
-            car2.setDamping(0.8, 0.8);
+            car2.geometry.computeBoundingBox();
         });
            
 
 
         jsonLoader.load( "blender/level.json", function( geometry, materials ) { 
-            plane = new Physijs.ConvexMesh( geometry, new THREE.MeshFaceMaterial(materials), 0 );
+            plane = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials), 0 );
             carpos = new THREE.Object3D();
             //Add the model to the scene
-            plane.position.y = -1;
             scene.add(plane);
 
         } );
@@ -121,8 +110,6 @@ document.getElementById('container').addEventListener("mousemove", mouseMove);
     }
     var lastTime = 0;
     function animate() {
-        scene.simulate();
-
         var timeNow = new Date().getTime();
         if (lastTime != 0) {
             var elapsed = timeNow - lastTime;
@@ -146,7 +133,7 @@ var currentlyPressedKeys = {};
 function handleKeyDown(event) {
     currentlyPressedKeys[event.keyCode] = true;
     if (event.keyCode == 32) {
-        car.setLinearVelocity( new THREE.Vector3(0,10,0) );
+        //car.setLinearVelocity( new THREE.Vector3(0,10,0) );
     }
 }
 
@@ -156,70 +143,56 @@ function handleKeyUp(event) {
 }
 
 
-var extrarot = 0 // FIXME
+function collisionDetection() {
+    car.updateMatrixWorld();
+    var center = car2.worldToLocal( car.localToWorld(car.geometry.boundingSphere.center.clone()) );
+    var r = car.geometry.boundingSphere.radius;
+    var bb = car2.geometry.boundingBox;
+    console.log((center.x-r));
+    console.log(bb.max.x);
+    console.log(center);
+    console.log(r);
+    if ((center.x + r) > bb.min.x && (center.x - r) < bb.max.x &&
+        (center.y + r) > bb.min.y && (center.y - r) < bb.max.y &&
+        (center.z + r) > bb.min.z && (center.z - r) < bb.max.z) {
+        return true;
+    }
+    return false;
+}
+
+
 function handleKeys(elapsed) {
-    var speed = 4;
-    var rspeed = 2;
     if (car) {
-    var vector = new THREE.Vector3( 0, 0, -1 );
-    vector.applyEuler( car.rotation, car.eulerOrder );
-    vector.y = 0;
-    var cameraVector = new THREE.Vector3( 0, 0, -1 );
-    cameraVector.applyEuler( camera_pivot.rotation, camera_pivot.eulerOrder );
-    cameraVector.y = 0;
-    if (currentlyPressedKeys[37]) {
-        car.setAngularVelocity( new THREE.Vector3(0,1,0) );
-    }
-    else if (currentlyPressedKeys[39]) {
-        car.setAngularVelocity( new THREE.Vector3(0,-1,0) );
-    }
-    else {
-        car.setAngularVelocity( new THREE.Vector3(0,0,0) );
-    }
-    if (currentlyPressedKeys[40]) {
-        car.applyCentralForce( vector.multiplyScalar(-100) );
-    }
-    if (currentlyPressedKeys[38]) {
-        car.applyCentralForce( vector.multiplyScalar(100) );
-    }
-    if (currentlyPressedKeys[90]) {
-        camera_pivot.rotation.y -= elapsed*rspeed/1000;
-    }
-    if (currentlyPressedKeys[88]) {
-        camera_pivot.rotation.y += elapsed*rspeed/1000;
-    }
-    if (currentlyPressedKeys[89]) {
-        //console.log(vector);
-    }
-    if (currentlyPressedKeys[87]) {
-        car.setLinearVelocity( vector.multiplyScalar(10)  );
-    }
-    if (currentlyPressedKeys[82]) {
-        car.setLinearVelocity( vector.multiplyScalar(-10) );
-    }
-    vector.cross( new THREE.Vector3(0,1,0) );
-    if (currentlyPressedKeys[65]) {
-        car.setLinearVelocity( vector.multiplyScalar(-10) );
-    }
-    if (currentlyPressedKeys[83]) {
-        car.setLinearVelocity( vector.multiplyScalar(10) );
-    }
-    /*if (!currentlyPressedKeys[87] && !currentlyPressedKeys[65]
-        && !currentlyPressedKeys[82] && !currentlyPressedKeys[83]) {
-        car.setLinearVelocity( 3 );
-    }*/
-        //camera.position.z = car.position.z + 3 * Math.cos(car.rotation.y + extrarot);
-        //camera.position.x = car.position.x + 3 * Math.sin(car.rotation.y + extrarot);
-        //camera.position.y = car.position.y + 3;
-        /*
-        camera.position = car.localToWorld( new THREE.Vector3(0,0,5) );
-        camera.position.y += 1;
-        v2 = vector.clone();
-        v2.y = 0;
-        console.log(car.rotation);
-        v2.add(camera.position);
-        camera.lookAt(v2);
-        //camera.rotation.y = car.rotation.y + extrarot;
-        */
+        var speed = 10;
+        var vector = new THREE.Vector3( 0, 0, -1 );
+        vector.applyEuler( car.rotation, car.rotation.order );
+        vector.y = 0;
+        var cameraVector = new THREE.Vector3( 0, 0, -1 );
+        cameraVector.applyEuler( camera_pivot.rotation, camera_pivot.rotation.order );
+        cameraVector.y = 0;
+        if (currentlyPressedKeys[87] || currentlyPressedKeys[82] || currentlyPressedKeys[65] || currentlyPressedKeys[83]) {
+            old = car.position.clone();
+            if (currentlyPressedKeys[87]) {
+                car.position.add( vector.multiplyScalar(elapsed*speed/1000)  );
+            }
+            if (currentlyPressedKeys[82]) {
+                car.position.add( vector.multiplyScalar(-elapsed*speed/1000)  );
+            }
+            vector.cross( new THREE.Vector3(0,1,0) );
+            if (currentlyPressedKeys[65]) {
+                car.position.add( vector.multiplyScalar(-elapsed*speed/1000)  );
+            }
+            if (currentlyPressedKeys[83]) {
+                car.position.add( vector.multiplyScalar(elapsed*speed/1000)  );
+            }
+            console.log(car.position);
+            if (collisionDetection()) {
+                car.position = old;
+                console.log('MOO');
+            }
+            console.log(old);
+            console.log(car.position);
+            console.log(' ');
+        }
     }
 }
